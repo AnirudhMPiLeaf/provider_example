09648f625aaad7605367a7197258141325cdabca
AnirudhMPiLeaf <anirudh.pai@ileafsolutions.net>
2023-03-17 15:05:43 +0530

    connectivity and focus functionalities added

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:provider_example/provider/connectivity.dart';
 import 'package:provider_example/provider/counter.dart';
 import 'package:provider_example/routes/app_routes.dart';
 import 'package:provider_example/screens/home.dart';
+import 'package:provider_example/screens/no_internet.dart';
 
 GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
@@ -12,6 +15,9 @@ void main() {
     providers: [
       ChangeNotifierProvider(
         create: (context) => CounterProvider(context, 1),
+      ),
+      ChangeNotifierProvider(
+        create: (context) => ConnectivityProvider(),
       )
     ],
     child: const MyApp(),
@@ -28,7 +34,47 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(primarySwatch: Colors.blue),
       navigatorKey: navigatorKey,
       onGenerateRoute: AppRoutes().onGenerateRoute,
-      home: const HomeScreen(),
+      builder: (context, child) => StreamBuilder<ConnectivityResult>(
+        stream: Provider.of<ConnectivityProvider>(context).connectivityStream,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            if (isNetworkAvailable(snapshot)) {
+              return const _Unfocus(child: HomeScreen());
+            } else {
+              return const NoInternetScreen();
+            }
+          } else {
+            return const NoInternetScreen();
+          }
+        },
+      ),
+    );
+  }
+
+  bool isNetworkAvailable(AsyncSnapshot<ConnectivityResult> snapshot) {
+    return snapshot.data == ConnectivityResult.mobile ||
+        snapshot.data == ConnectivityResult.wifi;
+    // || snapshot.data == ConnectivityResult.vpn ||
+    // snapshot.data == ConnectivityResult.ethernet;
+  }
+}
+
+class _Unfocus extends StatelessWidget {
+  const _Unfocus({
+    Key? key,
+    required this.child,
+  }) : super(key: key);
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      behavior: HitTestBehavior.opaque,
+      onTap: () {
+        FocusManager.instance.primaryFocus?.unfocus();
+      },
+      child: child,
     );
   }
 }
--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:provider_example/screens/consumer.dart';
 import 'package:provider_example/screens/home.dart';
+import 'package:provider_example/screens/no_internet.dart';
 
 class AppRoutes {
   Route<dynamic> onGenerateRoute(RouteSettings routeSettings) {
@@ -12,6 +13,8 @@ class AppRoutes {
         return MaterialPageRoute(builder: (context) => const HomeScreen());
       case 'consumer':
         return MaterialPageRoute(builder: (context) => const ConsumerScreen());
+      case 'no-internet':
+        return MaterialPageRoute(builder: (context) => const NoInternetScreen());
       default:
         return MaterialPageRoute(builder: (context) => const HomeScreen());
     }
